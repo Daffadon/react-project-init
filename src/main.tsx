ff5041d75345df7dc88e7ccab6cf781602bf2267
Daffadon <daffaputranarendra9@gmail.com>
2024-03-23 11:05:53 +0700

    fix: add context provider for routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
 import Router from "./routes/Route";
+import ContextProvider from "./context/userContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router />
+    <ContextProvider>
+      <Router />
+    </ContextProvider>
   </React.StrictMode>
 );
