621a251a4c657bdb3af2f3fa4f63caa690138a40
Daffadon <daffaputranarendra9@gmail.com>
2024-04-02 02:05:42 +0700

    middleware: Add CSRF security layer to Axios instance
    This commit give new header to axiosClient as Axios instance
    which is csrf-token header. In the response interceptor,
    The logic still need to be tested

--- src/middleware/axios-client.ts
+++ src/middleware/axios-client.ts
@@ -1,23 +1,37 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const axiosClient = axios.create({
   baseURL: `${import.meta.env.VITE_APP_BASE_URL}/api`,
+  withCredentials: true
 });
 
-axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("ACCESS_TOKEN");
-  config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('ACCESS_TOKEN');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    if (config.method !== 'get') {
+      config.headers['csrf-token'] = localStorage.getItem('csrf-token');
+    }
+    return config;
+  },
+  (error) => {
+    throw error;
+  }
+);
 
 axiosClient.interceptors.response.use(
   (response) => {
+    if (response.config.method !== 'get') {
+      localStorage.setItem('csrf-token', response.data?.csrfToken);
+    }
     return response;
   },
   (error) => {
     const { response } = error;
     if (response.status === 401) {
-      localStorage.removeItem("ACCESS_TOKEN");
+      localStorage.removeItem('ACCESS_TOKEN');
     }
     throw error;
   }
